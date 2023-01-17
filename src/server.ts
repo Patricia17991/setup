c7af3b13c9402fd11c108e9049ce5e1f9cf0cc8d
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2023-01-17 13:39:17 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,21 @@
 import Fastify from 'fastify'
+import cors from '@fastify/cors'
 import {PrismaClient} from '@prisma/client' //accessing the database
 
 
 const app = Fastify()
 const prisma = new PrismaClient()
 
+app.register(cors)
+
 app.get('/', async() => {
     const habits = await prisma.habit.findMany({
-        where: {
-            title: {
-                startsWith: 'Beber'
-            }
-        }
+
     }) //returning the table habites
 
     return habits
 })
 
-
-
-
-app.listen({
-    port: 3333,
-}).then(() => {
-    console.log('HTTP Server running!')
-})
 
 
 
